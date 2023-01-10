49cb087c87a5506913696b15e4bb388d19eda1b7
VKamble61 <varsha.kamble61@gmail.com>
2023-01-10 10:42:32 +0000

    Add project files.

---
+++ ClientApp/src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { BooksComponent } from './components/books/books.component';
+import { DeleteBookComponent } from './components/delete-book/delete-book.component';
+import { NewBookComponent } from './components/new-book/new-book.component';
+import { ShowBookComponent } from './components/show-book/show-book.component';
+import { UpdateBookComponent } from './components/update-book/update-book.component';
+import { CounterComponent } from './counter/counter.component';
+import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { HomeComponent } from './home/home.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { BookService } from './services/book.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavMenuComponent,
+    HomeComponent,
+    CounterComponent,
+    FetchDataComponent,
+    BooksComponent,
+    DeleteBookComponent,
+    UpdateBookComponent,
+    NewBookComponent,
+    ShowBookComponent
+  ],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'counter', component: CounterComponent },
+      { path: 'fetch-data', component: FetchDataComponent },
+    ])
+  ],
+  providers: [BookService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
